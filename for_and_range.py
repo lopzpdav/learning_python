855d158190c9522cdf146adf640839e6e99cfd97
Patricio López <lopzpdav@gmail.com>
2024-12-08 22:27:50 -0500

    PLOPEZ - end learning range

--- for_and_range.py
+++ for_and_range.py
@@ -34,3 +34,19 @@ print(list(range(-10, -100, -50))) # works with negative values too
 text_list = ['Patricio', 'had', 'a', 'headache']
 for i in range(len(text_list)): # we can iterate using lenght function
     print(i, text_list[i])
+
+print(range(0,10)) # we can't print a range directly
+
+# range isn't an actual list, it's a lazy object who generates numbers
+# when needed, not store the numbers
+# we can iterate over a range or use index o checking membership "in"
+
+# the use of range is because of memory efficiency
+range(1, 100000000) # this is lightweight in memory
+list(range(1, 100000000)) # this consumes a lot of memory
+
+# the term "lazy" means that the numbers are generated on-demand
+# as you iterate, rather than pre-storing them
+
+# On the other side, we can use range as argument for functions
+print(sum(range(0, 4))) # 0 + 1 + 2 + 3
